8b10b692c33c226066a1ffad1db615858f473ab4
Sebastian Chwal <chwal.sebastian@gmail.com>
2017-06-18 14:13:11 +0200

    Initial commit

---
+++ desktop/src/com/agonia/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.agonia.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agonia.game.Agonia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Agonia(), config);
+	}
+}
